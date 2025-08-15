03421063d3f72a517861beea8f5c2e6ab0dfe88b
Hemant <hemant@Hemants-MacBook-Air.local>
2025-08-15 07:41:49 +0530

    mt fix

--- src/components/features/VegetableResultsLoader.js
+++ src/components/features/VegetableResultsLoader.js
@@ -7,7 +7,7 @@ import { CardPlaceholder } from "@/components/common/Placeholder";
 const VegetableResultsLoader = memo(function VegetableResultsLoader() {
   // Create an array of 8 placeholders
   return (
-    <Row className="g-4 pb-4">
+    <Row className="g-4 pb-4 mt-1">
       {[...Array(8)].map((_, index) => (
         <Col key={index} sm={6} lg={4} xl={3}>
           <CardPlaceholder showImage={true} imageHeight="200px" lines={4} />
